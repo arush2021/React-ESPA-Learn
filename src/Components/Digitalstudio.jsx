4dec835c99879118bf1be5a81e2f716b5d720762
keshav203 <keshavsharma203@gmail.com>
2020-10-17 14:27:33 +0530

    second commit

--- src/Components/Digitalstudio.jsx
+++ src/Components/Digitalstudio.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import aboutimg from '../images/aboutimg.jpg'
 import logo from '../images/logo.jpeg'
+import digitalvid from '../images/digitalvid.jpg'
+
+import industrial from '../images/industrial.jpg'
+import news from '../images/news.jpg'
+import edu from '../images/Banner5.jpeg'
 import digitalimg from '../images/digital.jpg'
 const service=[{
-   name: 'news video', para: 'Some make up the bulk of the card ' },
-  { name: 'education video', para: 'Some make up the bulk of the card 2' },
-  { name: 'digital content', para: 'Some make up the bulk of the card 2' },
-  { name: 'content writing', para: 'Some make up the bulk of the card 2' },
-  { name: 'portfolio shoot', para: 'Some make up the bulk of the card 2' },
-  { name: 'personal shoot', para: 'Some make up the bulk of the card 2' },
-  { name: 'portfolio', para: 'Some make up the bulk of the card 2' },
-  { name: 'industrial', para: 'Some make up the bulk of the card 3' 
+   name: 'News video', imgsrc:news },
+  { name: 'Education video',imgsrc:edu },
+  { name: 'Digital content',imgsrc:digitalvid },
+ 
+  { name: 'Portfolio shoot',imgsrc:industrial 
+
 }];
 function Digitalstudio() {
     return (
@@ -42,7 +45,7 @@ function Digitalstudio() {
             <div className="col-md-7 quot">
              
               <h4>About us</h4>
-               <p style={{fontSize:25}}>At Espa Digital Studio, we provide a wide range of creative, professional video & photography services to meet our customer's 1500s, when an unknown printer took  but also the leap into electronic   . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+               <p style={{fontSize:25}}>At Espa Digital Studio, we provide a wide range of creative, professional video & photography services to meet our customer's requirement.We take pride in being a leading digital agency and among the few independent digital outfits in India with a vast and reputed clientele of well-known brands.</p>
            
             </div>
         </div>
@@ -55,10 +58,10 @@ function Digitalstudio() {
         {service.map(servicedata => 
 <div  key={servicedata} className="col-md-3 ">
             <div class="card bg-light" style={{width: '18rem'}}>
-  <img src={aboutimg} class=" img-fluid" alt="services"/>
+  <img src={servicedata.imgsrc} class=" img-fluid" alt="services"/>
   <div class="card-body">
       <h4 class="mt-2">{servicedata.name}</h4>
-    <p class="card-text">{servicedata.para}</p>
+   
   </div>
 </div>
             </div>
@@ -75,7 +78,7 @@ function Digitalstudio() {
             <div className="col-md-7 quot">
              
               <h4>Why choose us</h4>
-               <p style={{fontSize:25}}>At Espa Digital Studio, we provide a wide range of creative, professional video & photography services to meet our customer's 1500s, when an unknown printer took  but also the leap into electronic   . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+               <p style={{fontSize:25}}> What makes us one of the best digital marketing agencies in India? It’s our legacy of being one of India’s largest independent digital agencies. Our team works closely with our extended network across the globe to develop and execute clever online and digital solutions to bring our clients users,</p>
            
             </div>
             <div className="col-md-5">
